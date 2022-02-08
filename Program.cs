c2143e660521525d3d15792936c425974ff00d74
Viney <vineykhaneja999@gmail.com>
2022-02-08 15:21:00 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class EmployeeWage
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage computation Program");
+            Console.ReadLine();
+
+        }
+    }
+}
